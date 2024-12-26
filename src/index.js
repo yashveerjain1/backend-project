ce198c986987de3b3ce3908a31b2ca31af56bc1c
yash veer jain <yashjain0112233@gmail.com>
2024-12-26 18:54:27 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import connectDB from './db/index.js';
 
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(
+    app.listen(process.env.PORT ,()=>{
+        console.log(`Server Is Runnig at Port: ${process.env.PORT}`);
+
+    })   
+
+)
+.catch((err)=>{
+    console.log("err",err);
+    
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// ! Promises method mostly used in the industry
+const asyncHandler=(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+
+} 
+export {asyncHandler};
+
+
+// // ?try catch method 
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+    
+// }
