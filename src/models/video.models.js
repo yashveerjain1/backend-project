54664739568f3235b881d7154fadb166eb5f7ee6
yash veer jain <yashjain0112233@gmail.com>
2024-12-26 21:15:29 +0530

    models Created :User  and video

---
+++ src/models/video.models.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema=new mongoose.Schema({
+        videoFile:{
+            type:String, //cloudinary url
+            required:true,
+
+        },
+        thumbnail:{
+            type:String,
+            required:true,
+
+        },
+        title:{
+            type:String,
+            required:true,
+
+        },
+        decription:{
+            type:true,
+            required:true,
+        },
+        duration:{
+            type:Number,
+            required:true,
+
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true,
+        },
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        }
+    
+});
+
+
+// agrregation pipelines ke use hota hai 
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+export const video=mongoose.model("video",videoSchema);
